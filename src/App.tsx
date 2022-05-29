06f50b4e144356c729ef2ee2fc08daa04b958311
Sebastian Carrillo <scarrillo@lya-electronic.com>
2022-05-28 22:53:11 -0500

    First logic bands components

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BandAdd } from "./components/BandAdd";
+import { BandList } from "./components/BandList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+    <div className="container">
+      <div className="alert">
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+          Service status:
+          <span className="text-success"> Online</span>
+          <span className="text-danger"> Offline</span>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      </div>
+      <h1>BandNames</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">{<BandList />}</div>
+        <div className="col-4">{<BandAdd />}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
